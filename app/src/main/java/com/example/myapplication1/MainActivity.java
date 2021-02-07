8cee541a247a9c325d91e06e9ddf17a3f1e2d8e9
Rahaf-439200720 <439200720@student.ksu.edu.sa>
2021-02-08 02:52:19 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myapplication1/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.myapplication1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.BreakIterator;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button b;
+    EditText e;
+    TextView t;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        b = (Button) findViewById(R.id.button);
+        e = (EditText) findViewById(R.id.PersonName);
+        t = (TextView) findViewById(R.id.view2);
+
+        b.setOnClickListener(new View.OnClickListener() {
+                                 @Override
+                                 public void onClick(View view) {
+                                     String st = "Hello, "+e.getText().toString();
+                                     t.setText(st);
+                                 }
+                             }
+        );
+    }
+}
